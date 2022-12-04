5962db7a49b9bf3a8e9d965176297bd0aa0765cd
Payam Yektamaram <payamyek@gmail.com>
2022-12-04 00:34:18 -0500

    feat: day 4 solutions with no explanations

--- solutions/day4/partA/solution.py
+++ solutions/day4/partA/solution.py
@@ -7,3 +7,24 @@ FILE_MODE_REAL_INPUT_MODE = True  # set to true to use problem input file 'input
 # open file for reading
 with open('input.txt' if FILE_MODE_REAL_INPUT_MODE else 'test.txt', 'r') as f:
     lines = f.read().splitlines()
+    lines = [line.split(',') for line in lines]
+
+# parse data into string tuples that depict the two ranges
+sections = [(tuple(x.split("-")), tuple(y.split("-"))) for x, y in lines]
+
+# running total
+total_sum = 0
+
+for x, y in sections:
+    # get range start and endpoints for first pair
+    x1, x2 = int(x[0]), int(x[1])
+
+    # get range start and endpoints for second pair
+    y1, y2 = int(y[0]), int(y[1])
+
+    # check if either range of sections is a subset of the other
+    if y1 <= x1 <= x2 <= y2 or x1 <= y1 <= y2 <= x2:
+        total_sum += 1
+
+# print result
+print(total_sum)
--- solutions/day4/partB/solution.py
+++ solutions/day4/partB/solution.py
@@ -7,3 +7,27 @@ FILE_MODE_REAL_INPUT_MODE = True  # set to true to use problem input file 'input
 # open file for reading
 with open('input.txt' if FILE_MODE_REAL_INPUT_MODE else 'test.txt', 'r') as f:
     lines = f.read().splitlines()
+    lines = [line.split(',') for line in lines]
+
+# parse data into string tuples that depict the two ranges
+sections = [(tuple(x.split("-")), tuple(y.split("-"))) for x, y in lines]
+
+# running total
+total_sum = 0
+
+for x, y in sections:
+    # get range start and endpoints for first pair
+    x1, x2 = int(x[0]), int(x[1])
+
+    # get range start and endpoints for second pair
+    y1, y2 = int(y[0]), int(y[1])
+
+    # convert ranges to actual sts
+    x_range = set(range(x1, x2+1))
+    y_range = set(range(y1, y2+1))
+
+    # check if intersection exists
+    if len(x_range & y_range):
+        total_sum += 1
+
+print(total_sum)
